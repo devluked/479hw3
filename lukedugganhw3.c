6c0bbb4f3d45924029c81ae3f1ae8a6ad3345dd2
Luke <devlukeduggan@gmail.com>
2021-03-03 10:37:56 -0800

    fiish prog

--- lukedugganhw3.c
+++ lukedugganhw3.c
@@ -13,15 +13,13 @@ MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 MPI_Comm_size(MPI_COMM_WORLD,&size);
 
 srand(rank);
-int cont[size-1];
 int num = ((rand() % (9)+1));
 if (num%2==0)
   num+=1;
-cont[rank] = num;
 
 
 MPI_Scan(&num, &b, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-printf("Process %d will recieve the array portion between index %d-%d\n", rank, b-num+1, b);
+printf("Process %d is asking for %d and will recieve the array portion between index %d-%d\n", rank, num, b-num+1, b);
 
 
 MPI_Finalize();
